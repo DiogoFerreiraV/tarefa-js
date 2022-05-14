5cddc46d5d1331cd7f369b172d3526e84acd0f71
DiogoFerreiraV <Diogoferreirav21@outlook.com>
2022-05-14 16:54:10 -0300

    tarefa presencial 3

---
+++ js/2.js
@@ -0,0 +1,92 @@
+//alert("Seja Bem vindo!!");
+//document.write("Bem vindo ao Javascript");
+//prompt("digite seu nome");
+//resp = confirm("Será que vai chover?")
+//alert("Um retorno de carro\bem no meio desta lina");
+//alert("\"Isto não saiu como deveria!\"disse ela");
+//alert("esta linha tem tubulação\taqui");
+
+/*var a, b;
+a=5;
+b="5 Kg de açucar";
+document.write("O valor da variável A é:"+a);
+document.write("<p>"+"O vaçor da variável B é:"+b+"</p>");
+document.write("<p>"+"O valor da variável A dividido por 2 é:"+a/2+"</p>");
+document.write("<p>"+ "O valor da variável B dividido por 2 é:"+b/2+"</p>");*/
+ 
+/*var a, inteiro, flutuante;
+a="45.78 Kg de açucar";
+inteiro=parseInt(a);
+flutuante=parseFloat(a)
+document.write("O valor da variável A é:"+a);
+document.write("<p>"+"O valor numérico inteiro da variável é:"+inteiro+"</p>");
+document.write("<p>"+"O valor da variável A dividido por 2 é:"+inteiro/2+"</p>");
+document.write("<p>"+"O valor da variável decimal da variante é:"+flutuante+"</p>");*/
+
+/*var a, b, c;
+a="Ola pessoal";
+b="sejam bem vindos ao JavaScript";
+c=a+b;
+alert(c);*/
+
+/*var a, b, c, d, e;
+a=50/2;
+b=100*5;
+c=25*2;
+d=50-50;
+e=50%2;
+document.write("<p>"+"O valor da variável A é:"+a);
+document.write("<p>"+"O valor da variável B é:"+b);
+document.write("<p>"+"O valor da variável C é:"+c);
+document.write("<p>"+"O valor da variável D é:"+d);
+document.write("<p>"+"O valor da variável E é:"+e);*/
+
+/*hoje=new Date();
+xDia=hoje.getDay();
+pratoDia=new Date();
+valorPrato=new Date();
+diaSemana=new Array(7);
+
+//Dias da semana
+diaSemana[0]=" Domingo";
+diaSemana[1]=" Segunda-feira";
+diaSemana[2]=" Terça-feira";
+diaSemana[3]=" Quarta-feira";
+diaSemana[4]=" Quinta-feira";
+diaSemana[5]=" Sexta-feira";
+diaSemana[6]=" Sábado";
+
+//Pratos do dia
+pratoDia[0]=" Feijoada";
+pratoDia[1]=" Feijão Tropeiro";
+pratoDia[2]=" Arroz Carreteiro";
+pratoDia[3]=" Risoto";
+pratoDia[4]=" Lagosta";
+pratoDia[5]=" Picanha defumada";
+pratoDia[6]=" Carne de panela";
+
+//Preços dos pratos
+valorPrato[0]=" R$25,00 porção para 3";
+valorPrato[1]=" R$30,00 porção para 3";
+valorPrato[2]=" R$25,00 porção para 3";
+valorPrato[3]=" R$35,00 porção para 3";
+valorPrato[4]=" R$40,00 porção para 3";
+valorPrato[5]=" R$50,00 porção para 3";
+valorPrato[6]=" R$20,00 porção para 3";
+
+document.write(" O número da semana é:"+""+(xDia+1));
+document.write("<p><b>"+"Hoje é:"+diaSemana[xDia]);
+document.write("<p><b>"+"Prato do dia é:"+pratoDia[xDia]);
+document.write("<p><b>"+"Valor da porção:"+valorPrato[xDia]);*/
+
+/*for(i=1; i<5;i++){
+    document.write("Esta é a linha ",i,"<br>");
+}*/
+
+/*valor=new Array(3,2,4,1,5);
+i=0;
+total=0;
+while(total<10){
+    total+=valor[i];i++;
+}
+document.write("Total = "+total);*/
